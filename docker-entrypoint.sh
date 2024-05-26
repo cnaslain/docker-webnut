23ac553fd5ca1c62b59b350b97d534b3649917e8
cnaslain <cnaslain@gmail.com>
2024-05-26 20:43:22 +0200

    Use https://github.com/AdeMiller/webNUT.git for multi-server configuration and use an external config file.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -u -e -o pipefail
 
-upshost="${UPS_HOST:-127.0.0.1}"
-upsport="${UPS_PORT:-3493}"
-upsuser="${UPS_USER:-monuser}"
-upspassword="${UPS_PASSWORD:-secret}"
+if [ ! -f /app/webNUT/webnut/config.py ]; then
+    upshost="${UPS_HOST:-127.0.0.1}"
+    upsport="${UPS_PORT:-3493}"
+    upsuser="${UPS_USER:-monuser}"
+    upspassword="${UPS_PASSWORD:-secret}"
 
-echo "server = '$upshost'" > /app/webNUT/webnut/config.py
-echo "port = '$upsport'" >> /app/webNUT/webnut/config.py
-echo "username = '$upsuser'" >> /app/webNUT/webnut/config.py
-echo "password = '$upspassword'" >> /app/webNUT/webnut/config.py
+    echo "server = '$upshost'" > /app/webNUT/webnut/config.py
+    echo "port = '$upsport'" >> /app/webNUT/webnut/config.py
+    echo "username = '$upsuser'" >> /app/webNUT/webnut/config.py
+    echo "password = '$upspassword'" >> /app/webNUT/webnut/config.py
+fi
 
 cat /app/webNUT/webnut/config.py
 cd /app/webNUT && python setup.py install
